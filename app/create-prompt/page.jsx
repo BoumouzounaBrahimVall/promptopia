a6477cf762f05f9d0001e99d4899f2b1f5639814
BoumouzounaBrahimVall <brahimvall.boumouzouna@etu.fstm.ac.ma>
2023-10-01 19:36:26 +0100

    finished google auth and sign in

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+
+function CreatePrompt() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreatePrompt
